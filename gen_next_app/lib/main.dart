3cc139080f798a0a7a7e9a169059b865dc815f17
AayushDani <aayushdani1@gmail.com>
2020-03-22 16:17:15 +0530

    hope this works

--- gen_next_app/lib/main.dart
+++ gen_next_app/lib/main.dart
@@ -1,5 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {}
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text("GenNext Application"),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_drop_down_circle),
+            color: Colors.black87,
+            onPressed: () {},
+          ),
+          backgroundColor: Colors.blueAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.notifications),
+              color: Colors.black87,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
