4eb78276729ff9d145f6a3361f81ca49278c4fb3
Dhananjayan-PN <pndjay@gmail.com>
2020-03-22 19:25:47 +0530

    app look and feel

--- gen_next_app/lib/main.dart
+++ gen_next_app/lib/main.dart
@@ -7,12 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.grey[300],
         appBar: AppBar(
-          title: Center(
-            child: Text(
-              "GenNext Application",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
+          title: Text('GenNext'),
           leading: IconButton(
             icon: Icon(Icons.arrow_drop_down_circle),
             color: Colors.black,
@@ -26,6 +21,22 @@ void main() {
               onPressed: () {},
             ),
           ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage('images/doggo.png'),
+              ),
+              Text(
+                'Our Application is under development\nCome back soon!',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     ),
