ff9f61c4eb0988c513203e1012bbf293630ebfb3
Dhananjayan-PN <pndjay@gmail.com>
2020-03-23 20:10:19 +0530

    drawer update

--- gen_next_app/lib/main.dart
+++ gen_next_app/lib/main.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[250],
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Text(
+                'Your Universities',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Completed Applications',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Pending Applications',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Counselor Schedule',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Image(
             image: AssetImage('images/gennextlonglogo.png'),
           ),
-          leading: IconButton(
-            icon: Icon(Icons.view_headline),
-            color: Colors.black,
-            onPressed: () {},
-          ),
           backgroundColor: Colors.cyanAccent[400],
+          iconTheme: new IconThemeData(color: Colors.black),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.notifications),
@@ -41,6 +72,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
