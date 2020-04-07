40bce827c2d9782ab57787e6963bdeef49c8e5ed
Dhananjayan P N <pndjay@gmail.com>
2020-04-07 22:40:00 +0530

    login page modification

--- gen_next_app/lib/login.dart
+++ gen_next_app/lib/login.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'student/home.dart';
 
 class LoginPage extends StatefulWidget {
@@ -18,13 +20,13 @@ class _LoginPageState extends State<LoginPage> {
     if (form.validate()) {
       form.save();
       if (_email == "jake.adams@gmail.com" && _password == 'gennext') {
-        Navigator.push(context, new MaterialPageRoute(builder: (context) => StudentHomeScreen()));
+        Navigator.push(context, PageTransition(type: PageTransitionType.downToUp, child: StudentHomeScreen()));
       }
       if (_email == "counsellor@gennext.edu" && _password == 'gennext') {
-        //counsellor view
+        print('Hey Counsellor');
       }
       if (_email == "university@gennext.edu" && _password == 'gennext') {
-        //university view
+        print('Hey University');
       } else {
         print("Invalid");
       }
@@ -35,38 +37,196 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Container(
+    return Scaffold(
+      body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [Colors.white, Colors.blue]),
+          gradient: LinearGradient(begin: Alignment.topRight, end: Alignment.bottomLeft, colors: [Colors.blueGrey[500], Colors.cyan[500]]),
         ),
-        child: Container(
-            padding: EdgeInsets.all(16.0),
-            child: new Form(
+        child: ListView(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 60, left: 10),
+                  child: RotatedBox(
+                      quarterTurns: -1,
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0, left: 10.0),
+                  child: Container(
+                    //color: Colors.green,
+                    height: 200,
+                    width: 200,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 60,
+                        ),
+                        Center(
+                          child: Text(
+                            'A world of possibility in an app',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Form(
               key: formKey,
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+              child: Column(
                 children: <Widget>[
-                  new TextFormField(
-                    decoration: InputDecoration(labelText: "Email"),
-                    validator: (value) => value.isEmpty ? 'Enter a valid Email ID' : null,
-                    onSaved: (value) => _email = value,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50, left: 50, right: 50),
+                    child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        validator: (value) => value.isEmpty ? 'Enter a valid Email ID' : null,
+                        onSaved: (value) => _email = value,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          fillColor: Colors.lightBlueAccent,
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  new TextFormField(
-                    decoration: InputDecoration(labelText: "Password"),
-                    obscureText: true,
-                    validator: (value) => value.isEmpty ? 'Enter a valid Email ID' : null,
-                    onSaved: (value) => _password = value,
-                  ),
-                  new RaisedButton(
-                    color: Colors.blueGrey,
-                    child: new Text('Login'),
-                    onPressed: validateAndSave,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, left: 50, right: 50),
+                    child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        validator: (value) => value.isEmpty ? 'Enter a valid Email ID' : null,
+                        onSaved: (value) => _password = value,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40, right: 50, left: 200),
+              child: Container(
+                alignment: Alignment.bottomRight,
+                height: 47,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.cyan[800],
+                      blurRadius: 10.0, // has the effect of softening the shadow
+                      spreadRadius: 1.0, // has the effect of extending the shadow
+                      offset: Offset(
+                        5.0, // horizontal, move right 10
+                        5.0, // vertical, move down 10
+                      ),
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: FlatButton(
+                  splashColor: Colors.cyanAccent[400],
+                  onPressed: validateAndSave,
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 55, right: 55),
+              child: Container(
+                alignment: Alignment.topRight,
+                //color: Colors.red,
+                height: 25,
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Your first time here? ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    InkWell(
+                      splashColor: Colors.cyan.withAlpha(0),
+                      onTap: () {},
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+/*<Widget>[
+  TextFormField(
+  decoration: InputDecoration(labelText: "Email"),
+  validator: (value) => value.isEmpty ? 'Enter a valid Email ID' : null,
+  onSaved: (value) => _email = value,
+  ),
+  TextFormField(
+  decoration: InputDecoration(labelText: "Password"),
+  obscureText: true,
+  validator: (value) => value.isEmpty ? 'Enter a valid Email ID' : null,
+  onSaved: (value) => _password = value,
+  ),
+  RaisedButton(
+  color: Colors.cyan[300],
+  child: new Text('Login'),
+  onPressed: validateAndSave,
+  ),
+  ],
+*/
