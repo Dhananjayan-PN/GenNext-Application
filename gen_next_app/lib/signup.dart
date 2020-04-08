66b20c4a0ef3c8653df142765af9e9529423183e
Dhananjayan P N <pndjay@gmail.com>
2020-04-09 03:00:55 +0530

    sign up navigation

--- gen_next_app/lib/signup.dart
+++ gen_next_app/lib/signup.dart
@@ -10,71 +10,343 @@ class SignUpPage extends StatefulWidget {
 }
 
 class SignUpPageState extends State<SignUpPage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.bottomLeft, end: Alignment.topRight, colors: [Colors.cyan[300], Colors.blueGrey[800]]),
+    List<Widget> _pageOptions = <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomLeft, end: Alignment.topRight, colors: [Colors.cyan[300], Colors.blueGrey[800]]),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 200),
+              child: Text(
+                "Hi there !",
+                style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
             ),
-            child: Column(
+            Padding(
+              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: Text(
+                "Welcome to Gen Next Edu's App !",
+                style: TextStyle(color: Colors.white, fontSize: 23),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+              child: Text(
+                "Our goal is to help students like you dash through the college admission process, with the help of our talented team and this feature-packed app",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 50, left: 10, right: 10),
+              child: Text(
+                "Click start to begin your journey with us.\nBear in mind that none of your information\nwill be released without your permission",
+                style: TextStyle(color: Colors.white70, fontSize: 15),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Row(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(top: 250),
+                  padding: EdgeInsets.only(top: 30, left: 130),
                   child: Text(
-                    "Hi there !",
-                    style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.w500),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                  child: Text(
-                    "Welcome to Gen Next Edu's App !",
-                    style: TextStyle(color: Colors.white, fontSize: 23),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-                  child: Text(
-                    "Our goal is to help students like you dash through the college admission process, with the help of our talented team and this feature-packed app",
+                    "START",
                     style: TextStyle(color: Colors.white, fontSize: 18),
                     textAlign: TextAlign.center,
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 60, left: 15, right: 15),
-                  child: Text(
-                    "We're currently not registering new users for development purposes. Come back soon!",
-                    style: TextStyle(color: Colors.white70, fontSize: 15),
-                    textAlign: TextAlign.center,
+                  padding: EdgeInsets.only(top: 30, right: 15),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_forward,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex += 1;
+                      });
+                    },
                   ),
                 ),
-                Row(children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 10, left: 120),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.arrow_back,
-                        size: 30,
-                        color: Colors.cyan[100],
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 10, left: 0, right: 15),
-                    child: Text(
-                      "BACK",
-                      style: TextStyle(color: Colors.cyan[100], fontSize: 18),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ])
               ],
-            )));
+            ),
+          ],
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomLeft, end: Alignment.topRight, colors: [Colors.cyan[300], Colors.blueGrey[800]]),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 300),
+              child: Center(
+                child: Text(
+                  'Page 2',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 300, bottom: 5),
+              child: Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _selectedIndex -= 1;
+                            });
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Text(
+                          "BACK",
+                          style: TextStyle(color: Colors.white70, fontSize: 15),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 160, right: 0),
+                        child: Text(
+                          "NEXT",
+                          style: TextStyle(color: Colors.white70, fontSize: 15),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_forward,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _selectedIndex += 1;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomLeft, end: Alignment.topRight, colors: [Colors.cyan[300], Colors.blueGrey[800]]),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 300),
+              child: Center(
+                child: Text(
+                  'Page 2',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 300, bottom: 5),
+              child: Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _selectedIndex -= 1;
+                            });
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Text(
+                          "BACK",
+                          style: TextStyle(color: Colors.white70, fontSize: 15),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 160, right: 0),
+                        child: Text(
+                          "NEXT",
+                          style: TextStyle(color: Colors.white70, fontSize: 15),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_forward,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _selectedIndex += 1;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomLeft, end: Alignment.topRight, colors: [Colors.cyan[300], Colors.blueGrey[800]]),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 300),
+              child: Center(
+                child: Text(
+                  'Page 2',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 300, bottom: 5),
+              child: Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _selectedIndex -= 1;
+                            });
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Text(
+                          "BACK",
+                          style: TextStyle(color: Colors.white70, fontSize: 15),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 160, right: 0),
+                        child: Text(
+                          "NEXT",
+                          style: TextStyle(color: Colors.white70, fontSize: 15),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_forward,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _selectedIndex += 1;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      body: _pageOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Colors.cyan[500]),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fiber_manual_record),
+              title: Text(''),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fiber_manual_record),
+              title: Text(''),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fiber_manual_record),
+              title: Text(''),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fiber_manual_record),
+              title: Text(''),
+            ),
+          ],
+          unselectedItemColor: Colors.cyan[200],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.cyan[900],
+        ),
+      ),
+    );
   }
 }
