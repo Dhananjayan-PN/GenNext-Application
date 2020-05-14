75e4f507deaa7d2c951cb289b921f4e411da8b00
Dhananjayan P N <pndjay@gmail.com>
2020-05-14 18:58:54 +0530

    schedule screen for students

--- lib/student/schedule.dart
+++ lib/student/schedule.dart
@@ -1,6 +1,14 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:searchable_dropdown/searchable_dropdown.dart';
+import 'package:http/http.dart' as http;
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'home.dart';
 
 class ScheduleScreen extends StatefulWidget {
@@ -39,22 +47,7 @@ class ScheduleScreenState extends State<ScheduleScreen> {
           name: newUser.firstname + ' ' + newUser.lastname,
           email: newUser.email),
       appBar: CustomAppBar('Counselling Schedule'),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              image: AssetImage('images/doggo.png'),
-            ),
-            Text(
-              'Our Application is under development\nCome back soon!',
-              style: TextStyle(color: Colors.black, fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: Text('Calendar')),
     );
   }
 }
