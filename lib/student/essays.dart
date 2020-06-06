6921bc96557aa7a4aa90003244c0fd9cf5dc1391
Dhananjayan P N <pndjay@gmail.com>
2020-06-06 19:42:27 +0530

    updates

--- lib/student/essays.dart
+++ lib/student/essays.dart
@@ -4,6 +4,7 @@ import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:http/http.dart' as http;
 import 'package:quill_delta/quill_delta.dart';
+import 'dart:math';
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -16,10 +17,16 @@ class EssaysScreen extends StatefulWidget {
 }
 
 class _EssaysScreenState extends State<EssaysScreen> {
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+  TextEditingController controller = TextEditingController();
+  String filter;
+  Future essayList;
+
   @override
   void initState() {
     super.initState();
     BackButtonInterceptor.add(myInterceptor);
+    essayList = getEssays();
   }
 
   @override
@@ -38,6 +45,73 @@ class _EssaysScreenState extends State<EssaysScreen> {
     return true;
   }
 
+  void refresh() {
+    setState(() {
+      essayList = getEssays();
+    });
+  }
+
+  Future<void> getEssays() async {
+    final response = await http.get(
+      dom + 'api/student/get-my-essays',
+      headers: {HttpHeaders.authorizationHeader: "Token $tok"},
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['essay_data'];
+    } else {
+      throw 'failed';
+    }
+  }
+
+  Widget buildEssayCard(essay) {
+    return Padding(
+      padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        elevation: 10,
+        child: Material(
+          color: Colors.transparent,
+          child: ListTile(
+            key: Key(essay['essay_id'].toString()),
+            title: Text(
+              essay['essay_prompt'],
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        'University: ',
+                      ),
+                      Text(
+                        essay['university'].toString(),
+                        style: TextStyle(color: Colors.black54),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            trailing: Wrap(
+              children: <Widget>[
+                InkWell(
+                  child: Transform.rotate(
+                      angle: 3.14159, child: Icon(Icons.keyboard_backspace)),
+                  onTap: () {},
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -45,17 +119,127 @@ class _EssaysScreenState extends State<EssaysScreen> {
           name: newUser.firstname + ' ' + newUser.lastname,
           email: newUser.email),
       appBar: CustomAppBar('Essays'),
-      body: Center(
-        child: FlatButton(
-          color: Colors.blue,
-          child: Text('Essays'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              PageTransition(
-                type: PageTransitionType.fade,
-                child: EssayEditor(),
-              ),
+      body: RefreshIndicator(
+        key: refreshKey,
+        onRefresh: () {
+          refresh();
+          return essayList;
+        },
+        child: FutureBuilder(
+          future: essayList.timeout(Duration(seconds: 10)),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Padding(
+                padding: EdgeInsets.only(bottom: 40.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.error_outline,
+                        size: 30,
+                        color: Colors.red.withOpacity(0.9),
+                      ),
+                      Text(
+                        'Unable to establish a connection with our servers.\nCheck your connection and try again later.',
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }
+            if (snapshot.hasData) {
+              if (snapshot.data.length == 0) {
+                return Padding(
+                  padding: EdgeInsets.only(bottom: 70),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Opacity(
+                          opacity: 0.9,
+                          child: Image.asset(
+                            "images/snap.gif",
+                            height: 100.0,
+                            width: 100.0,
+                          ),
+                        ),
+                        Text(
+                          'Oh Snap!',
+                          style: TextStyle(fontSize: 18, color: Colors.black54),
+                        ),
+                        Padding(
+                            padding:
+                                EdgeInsets.only(top: 5, left: 30, right: 30),
+                            child: Text(
+                              "Looks like you haven't added\nany essays yet",
+                              style: TextStyle(color: Colors.black54),
+                              textAlign: TextAlign.center,
+                            )),
+                        Padding(
+                          padding: EdgeInsets.only(top: 3),
+                          child: Text(
+                              "Click the '+' to add one and get writing!",
+                              style: TextStyle(color: Colors.black54),
+                              textAlign: TextAlign.center),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: 5, left: 18, right: 30, bottom: 20),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 5, right: 6),
+                            child: Icon(
+                              Icons.search,
+                              size: 30,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  labelText: "Search",
+                                  contentPadding: EdgeInsets.all(2)),
+                              controller: controller,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return filter == null || filter == ""
+                              ? buildEssayCard(snapshot.data[index])
+                              : snapshot.data[index]['essay_prompt']
+                                      .toLowerCase()
+                                      .contains(filter)
+                                  ? buildEssayCard(snapshot.data[index])
+                                  : snapshot.data[index]['university']
+                                          .toLowerCase()
+                                          .contains(filter)
+                                      ? buildEssayCard(snapshot.data[index])
+                                      : Container();
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              }
+            }
+            return Center(
+              child: CircularProgressIndicator(),
             );
           },
         ),
@@ -73,7 +257,6 @@ class _EssayEditorState extends State<EssayEditor> {
   ZefyrController _controller;
   FocusNode _focusNode;
   Future<NotusDocument> loadDocument;
-  Future essayList;
 
   @override
   void initState() {
@@ -81,7 +264,6 @@ class _EssayEditorState extends State<EssayEditor> {
     _focusNode = FocusNode();
     loadDocument = _loadDocument();
     BackButtonInterceptor.add(myInterceptor);
-    essayList = getEssays();
   }
 
   @override
@@ -99,18 +281,6 @@ class _EssayEditorState extends State<EssayEditor> {
   Future<NotusDocument> _loadDocument() async {
     final Delta delta = Delta()..insert("Essay editor\n");
     return NotusDocument.fromDelta(delta);
-  }
-
-  Future<void> getEssays() async {
-    final response = await http.get(
-      dom + 'api/student/get-my-essays',
-      headers: {HttpHeaders.authorizationHeader: "Token $tok"},
-    );
-    if (response.statusCode == 200) {
-      return json.decode(response.body)['essay_data'];
-    } else {
-      throw 'failed';
-    }
   }
 
   Widget build(BuildContext context) {
