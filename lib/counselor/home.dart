4758ed57a3ae493c4fff12df9dcffed1c238bd0a
Dhananjayan P N <pndjay@gmail.com>
2020-07-19 02:13:47 +0530

    drawer configuration

--- lib/counselor/home.dart
+++ lib/counselor/home.dart
@@ -243,76 +243,86 @@ class NavDrawer extends StatelessWidget {
             },
           ),
           Column(mainAxisSize: MainAxisSize.min, children: navlist),
-          ListTile(
-            leading: Icon(Icons.power_settings_new,
-                size: 26, color: Colors.red[600]),
-            title: Text('Sign Out',
-                style: TextStyle(color: Colors.black, fontSize: 16.5)),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  contentPadding: EdgeInsets.all(0),
-                  elevation: 20,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                  content: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 30),
-                          child: Icon(
-                            Icons.power_settings_new,
-                            size: 40,
-                            color: Colors.red.withOpacity(0.9),
+          Padding(
+            padding: EdgeInsets.only(left: 6),
+            child: ListTile(
+              leading: Icon(Icons.power_settings_new,
+                  size: 26, color: Colors.red[600]),
+              title: Align(
+                alignment: Alignment(-1, 0),
+                child: Text('Sign Out',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                        fontSize: 16.5)),
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    contentPadding: EdgeInsets.all(0),
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                    content: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 30),
+                            child: Icon(
+                              Icons.power_settings_new,
+                              size: 40,
+                              color: Colors.red.withOpacity(0.9),
+                            ),
                           ),
+                          Padding(
+                            padding:
+                                EdgeInsets.only(left: 5, right: 5, top: 10),
+                            child: Text(
+                              'Are you sure you want to sign out?',
+                              style: TextStyle(
+                                  fontSize: 14.3,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w300),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(color: Color(0xff005fa8)),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 5, right: 5, top: 10),
-                          child: Text(
-                            'Are you sure you want to sign out?',
-                            style: TextStyle(
-                                fontSize: 14.3,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w300),
-                            textAlign: TextAlign.center,
-                          ),
-                        )
-                      ],
-                    ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text(
+                          'Sign out',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.of(context).pushAndRemoveUntil(
+                              logoutRoute(), (route) => false);
+                        },
+                      ),
+                    ],
                   ),
-                  actions: <Widget>[
-                    FlatButton(
-                      child: Text(
-                        'Cancel',
-                        style: TextStyle(color: Color(0xff005fa8)),
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    FlatButton(
-                      child: Text(
-                        'Sign out',
-                        style: TextStyle(color: Colors.red),
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                        Navigator.of(context).pushAndRemoveUntil(
-                            logoutRoute(), (route) => false);
-                      },
-                    ),
-                  ],
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
           Divider(),
         ],
