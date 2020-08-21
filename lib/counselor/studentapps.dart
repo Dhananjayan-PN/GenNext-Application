3aed6f5145254af9b13e4b4d2f0d27c0ef513191
Dhananjayan P N <pndjay@gmail.com>
2020-08-21 13:32:36 +0530

    application cards

--- lib/counselor/studentapps.dart
+++ lib/counselor/studentapps.dart
@@ -260,6 +260,173 @@ class _StudentApplicationsScreenState extends State<StudentApplicationsScreen> {
     }
   }
 
+  Widget buildAppCard(application) {
+    DateTime deadline = DateTime.parse(application["application_deadline"]);
+    var timeleft = DateTime.now().isBefore(deadline)
+        ? deadline.difference(DateTime.now()).inDays
+        : 'Passed';
+    Color timecolor =
+        timeleft is int && timeleft < 10 ? Colors.red : Colors.white;
+    if (timeleft is int) {
+      timeleft = timeleft.toString() + ' days';
+    }
+    Widget cardData(ImageProvider imageProvider, bool isError) => Container(
+          decoration: BoxDecoration(
+            color: isError ? Color(0xff005fa8) : null,
+            image: imageProvider != null
+                ? DecorationImage(
+                    alignment: Alignment.center,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withAlpha(100), BlendMode.darken),
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  )
+                : DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.35), BlendMode.dstIn),
+                    image: NetworkImage(
+                        'https://www.shareicon.net/data/512x512/2016/08/18/814358_school_512x512.png',
+                        scale: 12),
+                  ),
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: ListTile(
+              key: Key(application['application_id'].toString()),
+              title: Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Text(
+                  application['university'],
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.5,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              subtitle: Padding(
+                padding: EdgeInsets.only(top: 2, bottom: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          'Due: ',
+                          style: TextStyle(
+                            fontSize: 13.5,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          DateFormat.yMMMMd('en_US').format(deadline) +
+                              ' ($timeleft)',
+                          style: TextStyle(
+                              fontSize: 13.5,
+                              color: timecolor,
+                              fontWeight: timecolor == Colors.red
+                                  ? FontWeight.w600
+                                  : null),
+                        ),
+                      ],
+                    ),
+                    application["completion_status"]
+                        ? Text('Completed',
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13.5,
+                            ))
+                        : Text(
+                            'Pending',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 13.5,
+                            ),
+                          )
+                  ],
+                ),
+              ),
+              trailing: Padding(
+                padding: EdgeInsets.only(top: 9.5, right: 3),
+                child: PopupMenuButton(
+                  child: Icon(
+                    Icons.more_vert,
+                    color: Colors.white,
+                  ),
+                  itemBuilder: (BuildContext context) {
+                    return {'Edit', 'Delete'}.map((String choice) {
+                      return PopupMenuItem<String>(
+                        height: 35,
+                        value: choice,
+                        child: Text(choice,
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.w400)),
+                      );
+                    }).toList();
+                  },
+                  onSelected: (value) async {
+                    switch (value) {
+                      case 'Edit':
+                        // final List details = await Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => NewApplicationScreen(
+                        //       op: 'Edit',
+                        //       deadline: deadline,
+                        //       notes: application['application_notes'],
+                        //       university: application['university'],
+                        //     ),
+                        //   ),
+                        // );
+                        // editApplication(application, details[1], details[2]);
+                        // loading(context);
+                        break;
+                      case 'Delete':
+                        // _deleteApplication(application['application_id']);
+                        break;
+                    }
+                  },
+                ),
+              ),
+              onTap: () async {
+                // ignore: unused_local_variable
+                // final bool data = await Navigator.push(
+                //   context,
+                //   PageTransition(
+                //     type: PageTransitionType.rightToLeftWithFade,
+                //     child: SingleAppScreen(
+                //       application: application,
+                //     ),
+                //   ),
+                // );
+                // refresh();
+              },
+            ),
+          ),
+        );
+    return Hero(
+      tag: application['application_id'].toString(),
+      child: Card(
+        margin: EdgeInsets.only(top: 7, left: 15, right: 15, bottom: 7),
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        elevation: 6,
+        child: CachedNetworkImage(
+          imageUrl: application['image_url'] ??
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png',
+          placeholder: (context, url) => CardPlaceHolder(),
+          errorWidget: (context, url, error) => cardData(null, true),
+          imageBuilder: (context, imageProvider) =>
+              cardData(imageProvider, false),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -298,10 +465,16 @@ class _StudentApplicationsScreenState extends State<StudentApplicationsScreen> {
             completed = [];
             for (int i = 0;
                 i < snapshot.data['incomplete_application_data'].length;
-                i++) {}
+                i++) {
+              pending.add(buildAppCard(
+                  snapshot.data['incomplete_application_data'][i]));
+            }
             for (int i = 0;
                 i < snapshot.data['completed_application_data'].length;
-                i++) {}
+                i++) {
+              completed.add(
+                  buildAppCard(snapshot.data['completed_application_data'][i]));
+            }
             return ListView(
               children: [
                 Padding(
